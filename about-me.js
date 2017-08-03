a0ec52c9e46210ca8c34d82c9b1b267c586d5471
FrankSeaman <frankseamen@gmail.com>
2017-08-02 17:43:43 -0700

    lab 3 changes

--- about-me.js
+++ about-me.js
@@ -1,6 +1,8 @@
 alert('Welcome to my game, My name is Franklyn');
 console.log('How much do you know about me?');
 
+var tally = 0
+
 //Question 1
 var name = prompt('What is your name?');
 console.log('Hello ' + name);
@@ -12,12 +14,14 @@ while (begin !== true){
     confirm ('Please play ' + name);
     begin = confirm ('Do you want to play?');
     console.log(name + ' choose ' + begin );
-}
+ }
+
 //Question 3
 var hero = prompt('Which do I prefer, DC or Marvel?').toLowerCase();
 if ( hero === 'dc'){
+     tally ++
     alert(' Correct');
-    console.log('You choose wisely' + hero);
+    console.log('You choose wisely ' + hero);
 } else{
     alert(' Sorry wrong choice');
     console.log('Sorry wrong choice');
@@ -25,6 +29,7 @@ if ( hero === 'dc'){
 //question 4
 var weather = prompt('Do I prefer Summer or Winter').toLowerCase();
 if ( weather === 'winter'){
+     tally ++
     alert('Correct');
     console.log('Correct');
 } else {
@@ -34,9 +39,44 @@ if ( weather === 'winter'){
 //question 5
 var comp = prompt('Do I use an Apple or PC?').toLowerCase();
 if (comp === 'pc'){
+     tally ++
     alert('You are correct');
     console.log('You are correct');
 }else{
     alert('Never an Apple!');
     console.log('Never an Apple');
 }
+
+//Question 6
+
+for ( var i = 0; i < 3; i ++ ) {
+    var age = prompt('How old am I?');
+    if (age < 33){
+         age = prompt('too low, try again.');
+    }else if (age > 33){
+        age = prompt('too high, try again');
+    }
+    else{
+        tally ++
+        alert( 'You guessed it!');
+         console.log(' You guessed my age, ' + name);
+         break;
+
+    }
+}
+
+var stateArr = [ 'arizona', 'nevada', 'oregon', 'florida' ];
+for ( var i = 0; i < 6; i ++ ) {
+    var answer = prompt(" What States have I lived in? There are 4 answers").toLowerCase();
+    if ( answer === stateArr[0] || answer === stateArr[1] ||answer === stateArr[2] || answer === stateArr[3] ){
+       tally ++
+        alert('You guessed it Right.. but you did have 4 correct answers.');
+        console.log(' You guessed '  + answer );
+        break;
+    } else if (answer !== stateArr) {
+        alert( 'Choose again please!')
+        
+    }
+
+}
+console.log( tally );
